79678b87f18c488421685462474bd82b30406632
Alexander Guyer <guyera@oregonstate.edu>
2023-10-10 18:01:28 -0700

    Add structs

---
+++ structs/players.cpp
@@ -0,0 +1,44 @@
+// Task: Ask the user for an integer N. Create an array of N baseball_player
+// instances. Populate the array from data from the user.
+
+#include <iostream>
+
+using namespace std;
+
+struct baseball_player {
+	string first_name;
+	string last_name;
+	int age;
+	double batting_average;
+};
+
+int get_num_players() {
+	cout << "How many baseball players do you know? ";
+	int n;
+	cin >> n;
+	return n;
+}
+
+void init_players(baseball_player* players, int num_players) {
+	for (int i = 0; i < num_players; i++) {
+		// TODO (write and call a function here) Initialize the player at index
+		// i
+	}
+}
+
+int main() {
+	// Get n from the user
+	int num_players = get_num_players();
+	
+	baseball_player* players;
+	// TODO Create an array of N baseball players, where N is `num_players`.
+	// Store it in `players`. Hint: N is NOT a compile-time constant
+
+	// Initialize the players from input from the user
+	init_players(players, num_players);
+
+	// TODO (write and call a function here) Print the players' info
+
+	// TODO Is there something we need to do with `players` before it falls
+	// out of scope?
+}
---
+++ structs/team.cpp
@@ -0,0 +1,5 @@
+// Problem: A baseball team has a name and a roster of players. Modify your
+// solution to "players.cpp"---create a new struct type `baseball_team`,
+// which has a (dynamic) array of baseball_player instances and a name (string).
+// Create an array of exactly two teams, and populate them using inputs from
+// the user.
