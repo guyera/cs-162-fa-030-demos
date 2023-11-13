a8e5784377a1acfd24843f7e1dc1f9c87afad840
guyera <aguyer@chemeketa.edu>
2023-11-13 12:50:25 -0800

    assignment 3 help

--- assignment_3_help/roster.cpp
+++ assignment_3_help/roster.cpp
@@ -32,3 +32,35 @@ void roster::add_student(const student& s) {
 
 	students[n_students - 1] = s;
 }
+
+void roster::remove_student(int index) {
+	student* new_array = new student[n_students - 1];
+	for (int i = 0; i < n_students; i++) {
+		if (i < index) {
+			new_array[i] = students[i];
+		} else if (i > index) {
+			new_array[i - 1] = students[i];
+		}
+	}
+	delete [] students;
+	students = new_array;
+	n_students--;
+}
+
+void roster::operator=(const roster& other) {
+	// Free existing dyanmic memory
+	if (students != nullptr) {
+		delete [] students;
+	}
+
+	// Handle case where this == &other
+	if (this == &other) {
+		return;
+	}
+
+	n_students = other.n_students;
+	students = new student[n_students];
+	for (int i = 0; i < other.n_students; i++) {
+		students[i] = other.students[i];
+	}
+}
--- assignment_3_help/roster.h
+++ assignment_3_help/roster.h
@@ -13,6 +13,12 @@ public:
 	void load_data(ifstream& roster_file);
 	void print() const;
 	void add_student(const student& s);
+	void remove_student(int index);
+
+	void operator=(const roster& other);
+
+	// You'd also need a copy constructor and big 3,
+	// but I ran out of time in this demo
 };
 
 #endif
